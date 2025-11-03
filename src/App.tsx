9b3f31946d54799635d3712a6dcde38800995b4f
unknown <nnajivincentchidera@gmail.com>
2025-11-03 23:12:34 +0100

    Deploy updated build

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
+
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
